806b6810074bade4e9eb3b060034454fb0863c96
ido6251 <ido6251@gmail.com>
2025-03-05 16:09:59 +0200

    create template

--- app/main.py
+++ app/main.py
@@ -1,31 +1,8 @@
-import os
-from dotenv import load_dotenv
-from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
-from pydantic import BaseModel
-
-load_dotenv()
-
-DB_IP = os.getenv("DB_IP", "localhost")
-POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres") 
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres") 
-
-DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{DB_IP}"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+from fastapi import FastAPI
+from .api.endpoints.users import users_router
 
 app = FastAPI()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+app.include_router(users_router, prefix="/users")
 
 
 @app.get("/")
