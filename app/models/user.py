684fabf85c68d11320847aba039e1ad717a1a847
ido6251 <ido6251@gmail.com>
2025-03-06 01:30:07 +0200

    update import

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,6 @@
 from typing import List, Optional, Callable
 
-from sqlalchemy import Column, Integer, String, Boolean, Text, ForeignKey, CHAR, SQLColumnExpression, TIMESTAMP
+from sqlalchemy import Column, DateTime, Integer, String, Boolean, Text, ForeignKey, CHAR, SQLColumnExpression, TIMESTAMP
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import relationship, Mapped, Session
 
