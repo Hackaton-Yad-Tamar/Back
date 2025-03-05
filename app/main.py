db4a192846024f24f341085bffface86d2c60fcc
Shalevzzz <shalevaviv13@gmail.com>
2025-03-05 20:40:27 +0200

    Added CORS headers

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware  # Import CORS middleware
 
 from .api.endpoints.admin.dashboard import dashboard_router
 from .api.endpoints.users import users_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins (you can restrict this in production)
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, PATCH, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
+
 app.include_router(users_router, prefix="/users")
 app.include_router(dashboard_router, prefix="/admin/dashboard")
 
