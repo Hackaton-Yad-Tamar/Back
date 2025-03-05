2bc280dc6aa22d9d650e705f00355d1c7db512d2
Whitomtit <whitomtit@gmail.com>
2025-03-05 16:28:48 +0200

    Added routers for dashboard functions

--- app/core/adminview_db.py
+++ app/core/adminview_db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import func
-from models.request import Request, Request_Process
-from datetime import datetime
+
+from app.models.request import Request, RequestProcess
 
 '''
     The function below is used to get the count of requests based on the city, status and type of request.
@@ -12,15 +12,18 @@ from datetime import datetime
     type: the type of request
     The function returns the count of requests based on the city, status and type of request.
 '''
+
+
 def city_count(session, start_date, end_date, status=None, type=None):
     filters = [Request.created_at.between(start_date, end_date)]
-    
+
     if status:
         filters.append(Request.status == status)
     if type:
         filters.append(Request.request_type == type)
 
     return session.query(Request.city, func.count(Request.city)).filter(*filters).group_by(Request.city).all()
+
 
 '''
     The function below is used to get the count of status (open and closed) requests based on the city, and type of request.
@@ -32,15 +35,18 @@ def city_count(session, start_date, end_date, status=None, type=None):
     type: the type of request
     The function returns the count of status (open and closed) requests based on the city, and type of request.
 '''
+
+
 def status_count(session, start_date, end_date, city=None, type=None):
     filters = [Request.created_at.between(start_date, end_date)]
-    
+
     if city:
         filters.append(Request.city == city)
     if type:
         filters.append(Request.request_type == type)
 
     return session.query(Request.status, func.count(Request.status)).filter(*filters).group_by(Request.status).all()
+
 
 '''
     The function below is used to get the count of requests based on the type of request.
@@ -52,15 +58,19 @@ def status_count(session, start_date, end_date, city=None, type=None):
     type: the type of request
     The function returns the count of requests based on the type of request.
 '''
+
+
 def type_count(session, start_date, end_date, city=None, type=None):
     filters = [Request.created_at.between(start_date, end_date)]
-    
+
     if city:
         filters.append(Request.city == city)
     if type:
         filters.append(Request.request_type == type)
 
-    return session.query(Request.request_type, func.count(Request.request_type)).filter(*filters).group_by(Request.request_type).all()
+    return session.query(Request.request_type, func.count(Request.request_type)).filter(*filters).group_by(
+        Request.request_type).all()
+
 
 '''
     The function below is used to get the time taken to close a request based on the city, and type of request.
@@ -72,15 +82,18 @@ def type_count(session, start_date, end_date, city=None, type=None):
     type: the type of request
     The function returns the time taken to close a request based on the city, and type of request.
 '''
+
+
 def time_taken(session, start_date, end_date, city=None, type=None):
     filters = [Request.created_at.between(start_date, end_date), Request.status == 'closed']
-    
+
     if city:
         filters.append(Request.city == city)
     if type:
         filters.append(Request.request_type == type)
 
-    return session.query(Request.city, func.avg(func.date_diff(Request.created_at, Request.updated_at))).filter(*filters).group_by(Request.city).all()
+    return session.query(Request.city, func.avg(func.date_diff(Request.created_at, Request.updated_at))).filter(
+        *filters).group_by(Request.city).all()
 
 
 '''
@@ -93,15 +106,18 @@ def time_taken(session, start_date, end_date, city=None, type=None):
     request_type: the type of request
     The function returns the time taken to close a request based on the city, and type of request.
 '''
+
+
 def request_completion_time(session, start_date, end_date, city=None, request_type=None):
     filters = [Request.created_at.between(start_date, end_date)]
-    
+
     if city:
         filters.append(Request.city == city)
     if request_type:
         filters.append(Request.request_type == request_type)
 
     return session.query(
-        Request.request_id,
-        (func.extract('epoch', Request_Process.completed_at) - func.extract('epoch', Request.created_at)).label("completion_time")
-    ).join(Request_Process, Request.id == Request_Process.request_id).filter(*filters).all()
+        RequestProcess.request_id,
+        (func.extract('epoch', RequestProcess.completed_at) - func.extract('epoch', Request.created_at)).label(
+            "completion_time")
+    ).join(RequestProcess, Request.id == RequestProcess.request_id).filter(*filters).all()
--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+
+from .api.endpoints.admin.dashboard import dashboard_router
 from .api.endpoints.users import users_router
 
 app = FastAPI()
 app.include_router(users_router, prefix="/users")
+app.include_router(dashboard_router, prefix="/admin/dashboard")
 
 
 @app.get("/")
